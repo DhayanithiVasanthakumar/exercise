e50f4d2cbe63d4c5d4e1fbef093bf80d2cc983f8
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-08-04 20:28:46 +0530

    palindrome

---
+++ exercise/src/exercise/com/program/palindrome/Main.java
@@ -0,0 +1,10 @@
+package exercise.com.program.palindrome;
+
+public class Main {
+
+	public static void main(String[]args) {
+		Palindrome obj=new Palindrome();
+		String ans=obj.palindromeCheck("Malayalam");
+		System.out.println(ans);
+	}
+}
---
+++ exercise/src/exercise/com/program/palindrome/Palindrome.java
@@ -0,0 +1,10 @@
+package exercise.com.program.palindrome;
+
+public class Palindrome {
+	
+	public static String palindromeCheck(String str) {
+		String st=new StringBuffer(str).reverse().toString();
+		return st.equalsIgnoreCase(str)?"it is palindrome":"it is not palindrome";
+	}
+
+}
