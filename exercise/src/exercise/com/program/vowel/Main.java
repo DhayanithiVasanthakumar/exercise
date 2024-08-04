434192796f4cad4aa8341d11164f5cb92518980c
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-08-04 21:15:54 +0530

    vowels

--- exercise/src/exercise/com/program/vowel/Main.java
+++ exercise/src/exercise/com/program/vowel/Main.java
@@ -4,6 +4,18 @@ public class Main {
 
 	public static void main(String[]args) {
 		Vowel obj=new Vowel();
+		/* for vowel
 		String st=obj.vowelCheck("dhaya");
+		*/
+		
+		/*for vowel in string
+		boolean st=obj.vowelCheck("dhaya");
+		System.out.println(st);
+		*/
+		
+		// for vowel count
+		int value=obj.vowelCheck("dhaya");
+		System.out.println(value);
+		
 	}
 }
--- exercise/src/exercise/com/program/vowel/Vowel.java
+++ exercise/src/exercise/com/program/vowel/Vowel.java
@@ -2,7 +2,9 @@ package exercise.com.program.vowel;
 
 public class Vowel {
 
+	/* vowel
 	public static String vowelCheck(String str) {
+		
 		str.toLowerCase();
 		for(int i=1;i<str.length();i++) {
 			//charAt:select particular index number of string
@@ -11,6 +13,28 @@ public class Vowel {
 				System.out.println(ch);
 			}
 		}
-		return str;
+		return str;	
+	}
+	*/
+		
+	
+	/*vowel in string
+	public static boolean vowelCheck(String str) {
+		
+		return str.toLowerCase().matches(".*[aeiou].*");
+	}
+	*/
+	
+	//vowel count in string
+	public static int vowelCheck(String str) {
+		int count=0;
+		str=str.toLowerCase();
+		for(char ch:str.toCharArray()) {
+			if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+				count++;
+			}
+		}
+		return count;
+		
 	}
 }
