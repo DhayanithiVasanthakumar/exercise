bbb9b1d235b2e65da340f22c545d2ed4b0417ab4
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-08-06 22:53:06 +0530

    using recursion

--- exercise/src/exercise/com/program/fibonacci/Fibonacci.java
+++ exercise/src/exercise/com/program/fibonacci/Fibonacci.java
@@ -2,11 +2,11 @@ package exercise.com.program.fibonacci;
 
 public class Fibonacci {
 	
-	public static void fibonNumber(int num) {
+	public  int fibonNumber(int num) {
 	/*
 		fibonacci:frist 2 number is sum of third number
 		 after that 2nd number +sum of frist 2 number(third number)->fourth number......
-		 */
+		 
 		
 		int fristnumber=0,secondnumber=1;
 		for(int i=0;i<=num;i++) {
@@ -16,26 +16,22 @@ public class Fibonacci {
 			secondnumber=thirdnumber;
 		}
 		
+		*/
 		
-		
-	/*
+	
 	//using recursion
 	
-	int n=0;
-	for(int num=0;i<n;i++) {
-		System.out.println(fibonNumber(num));
-	}
-		
-	public static int fibonNumber(int num) {
-		if(num==0||num==1) {
+	
+	
+		if(num<=1) {
 			return num;
 		}
 		else {
 			return(fibonNumber(num-1)+fibonNumber(num-2));
 		}
+		
 	}
-	*/
 	
-	}
+}
 
-	}
+	
--- exercise/src/exercise/com/program/fibonacci/Main.java
+++ exercise/src/exercise/com/program/fibonacci/Main.java
@@ -3,9 +3,16 @@ package exercise.com.program.fibonacci;
 public class Main {
 
 	public static void main(String[]args) {
-		// feb normal
+		/* feb normal
 		Fibonacci obj=new Fibonacci();
 		obj.fibonNumber(8);
+		*/
+		
+		//recursion
+		Fibonacci obj=new Fibonacci();
+		for(int i=0;i<=10;i++) {
+			System.out.println(obj.fibonNumber(i));
+		}
 		
 		
 	
