37379f75bc9d5c0db8a021c0be6d56e7b1b1da72
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-08-09 21:00:22 +0530

    pattern

--- exercise/src/exercise/com/program/pattern/Main.java
+++ exercise/src/exercise/com/program/pattern/Main.java
@@ -2,7 +2,8 @@ package exercise.com.program.pattern;
 
 public class Main {
 	public static void main(String[]args) {
-		
+		Pattern obj=new Pattern();
+		obj.pattern(6);
 	}
 
 }
--- exercise/src/exercise/com/program/pattern/Pattern.java
+++ exercise/src/exercise/com/program/pattern/Pattern.java
@@ -1,5 +1,13 @@
 package exercise.com.program.pattern;
 
 public class Pattern {
+	public static void pattern(int num) {
+		for(int row=1;row<=num;row++) {
+			for(int col=1;col<=row;col++) {
+				System.out.println(row);
+			}
+			System.out.println();
+		}
+	}
 
 }
