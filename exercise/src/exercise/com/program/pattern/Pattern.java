211a1e9ec6f2b06d0689d065dfc95f402ba70cce
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-08-11 21:54:05 +0530

    pattern

--- exercise/src/exercise/com/program/pattern/Pattern.java
+++ exercise/src/exercise/com/program/pattern/Pattern.java
@@ -4,7 +4,7 @@ public class Pattern {
 	public static void pattern(int num) {
 		for(int row=1;row<=num;row++) {
 			for(int col=1;col<=row;col++) {
-				System.out.println(col);
+				System.out.println(row);
 			}
 			System.out.println();
 		}
