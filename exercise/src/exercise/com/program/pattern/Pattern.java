124bada9a9b576b9feac148bf8decb6c9c26badb
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-08-14 21:57:07 +0530

    pattern 3

--- exercise/src/exercise/com/program/pattern/Pattern.java
+++ exercise/src/exercise/com/program/pattern/Pattern.java
@@ -2,8 +2,9 @@ package exercise.com.program.pattern;
 
 public class Pattern {
 	public static void printPattern(int n){
-       
-		/* star 1
+       int row,col;
+		/* 
+		 //star 1
         for (int row = 1; row <= n; row++) {
 
             for (int col = 1; col <= row; col++) {
@@ -21,7 +22,7 @@ public class Pattern {
 *****
 ******
         
-        */
+        //star 2
 		for (int row = 6; row >= 1; row--) {
 
             for (int col = 1; col <= row; col++) {
@@ -31,8 +32,30 @@ public class Pattern {
             // printing new line for each row
             System.out.println();
         }
+	
 		
+******
+*****
+****
+***
+**
+*
+*/
 		
+		//star 3
+		
+       for(row=0; row<n; row++){
+           for(col=2*(n-row); col>=0; col--){
+               System.out.print(" ");
+           }
+           for(col=0; col<=row; col++){
+               System.out.print("* ");
+           }
+    
+           System.out.println();
+       }
+
+    
 		
 		
     }
