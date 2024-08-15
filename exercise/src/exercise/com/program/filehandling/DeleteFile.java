582767b01a207687cdf0fc0d3b0c30f4ec31e916
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-08-15 13:29:19 +0530

    Delete file

--- exercise/src/exercise/com/program/filehandling/DeleteFile.java
+++ exercise/src/exercise/com/program/filehandling/DeleteFile.java
@@ -1,8 +1,18 @@
 package exercise.com.program.filehandling;
 
+import java.io.File;
+
 public class DeleteFile {
 
 	public static void main(String[] args) {
+		File file =new File("C:\\Users\\dhaya\\OneDrive\\Desktop\\FileHandle\\subfolder\\test.txt");
+		//to delete file
+		file.delete();
+		
+		//to check the file is delete or not
+		boolean present=file.exists();
+		System.out.println("File present "+present);
+		
 		
 	}
 
