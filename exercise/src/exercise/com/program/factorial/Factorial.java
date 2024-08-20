29a1a2330d8836f4759054f48ef88d15935be87f
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-08-20 09:59:20 +0530

    fact

--- exercise/src/exercise/com/program/factorial/Factorial.java
+++ exercise/src/exercise/com/program/factorial/Factorial.java
@@ -1,5 +1,12 @@
 package exercise.com.program.factorial;
 
 public class Factorial {
-
+	public int factorial(int num) {
+		if(num==0) {
+			return 1;
+		}
+		else {
+			return num*factorial(num-1);
+		}
+	}
 }
--- exercise/src/exercise/com/program/factorial/Main.java
+++ exercise/src/exercise/com/program/factorial/Main.java
@@ -1,5 +1,8 @@
 package exercise.com.program.factorial;
 
 public class Main {
-
+	public static void main(String[]args) {
+		Factorial obj=new Factorial();
+		System.out.println(obj.factorial(5));
+	}
 }
