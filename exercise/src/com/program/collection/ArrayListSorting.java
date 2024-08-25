656e9e88fed8af20968288e53ed33220e82bc34b
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-08-25 14:44:14 +0530

    Comparator

--- exercise/src/com/program/collection/ArrayListSorting.java
+++ exercise/src/com/program/collection/ArrayListSorting.java
@@ -1,16 +1,19 @@
 package com.program.collection;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class ArrayListSorting {
 	public static void main(String[]args) {
+		//ascending order
 		
+		//primitive data type
 		int[] arr= {1,5,9,7,3,4,6,2,8,0};
 		
 		for(int i:arr) {
 			System.out.println(i);
 		}
-		
+		//In primitive data type .sort(primitive[] arr)
 		Arrays.sort(arr);
 		
 		System.out.println("after sorting");
@@ -18,19 +21,37 @@ public class ArrayListSorting {
 			System.out.println(i);
 		}
 		
-		String[] str= {"dhaya","naren","hari","asu","partha","pranesh"};
 		
-		for(String s:str) {
+		//non primitive data type 
+		String[] names= {"dhaya","naren","hari","asu","partha","pranesh"};
+		
+		for(String s:names) {
 			System.out.println(s);
 		}
 		
 		
-		
-		Arrays.sort(str);
+		// In non primitive data type .sort(Object[] s) 
+		Arrays.sort(names);
 		System.out.println("after sorting");
 		
-		for(String s:str) {
+		for(String s:names) {
 			System.out.println(s);
 		}
+		
+		//ascending order based on string length. so use comparator
+		
+		Comparator comp=new ComparatorDemo();
+		Arrays.sort(names,comp);
+		
+		System.out.println("****after compare method usage****");
+		
+		for(String s:names) {
+			System.out.println(s);
+		}
+		
+		
+		
+		
 	}
+	
 }
