9c9758212b3496eecb77d66dc14dc14747bcbb0a
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-08-26 19:54:41 +0530

    LinkedList

---
+++ exercise/src/com/program/collections/linkedlist/LinkedListDemo.java
@@ -0,0 +1,26 @@
+package com.program.collections.linkedlist;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		LinkedList ll=new LinkedList();
+		//offer() is same as add()
+		ll.offer(5);
+		ll.offer("dhaya");
+		ll.offerFirst('v');
+		ll.offerLast("nithi");
+		System.out.println(ll);
+		
+		//poll(),pollFrist(),pollLast(). is same as remove()
+		System.out.println("before poll"+ll);
+		System.out.println(ll.poll());
+		System.out.println("after poll"+ll);
+		
+		//clear() is used to remove all elements in entire list
+		ll.clear();
+		System.out.println(ll);
+	}
+
+}
