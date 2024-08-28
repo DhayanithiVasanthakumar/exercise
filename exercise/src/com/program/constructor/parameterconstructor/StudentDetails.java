d669f801fc9da6eec7e19aa96e5505d528c7247c
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-08-28 18:05:56 +0530

    added ques

--- exercise/src/com/program/constructor/parameterconstructor/StudentDetails.java
+++ exercise/src/com/program/constructor/parameterconstructor/StudentDetails.java
@@ -1,5 +1,13 @@
 package com.program.constructor.parameterconstructor;
 
+/*sample ques 
+  
+  Write a Java program to create a class called Dog with instance variables name and color. 
+  Implement a parameterized constructor that takes name and color as parameters and initializes
+   the instance variables. Print the values of the variables.
+ */
+
+
 public class StudentDetails {
 
 	private String name;
