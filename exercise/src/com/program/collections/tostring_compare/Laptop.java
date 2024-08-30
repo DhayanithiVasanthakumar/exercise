01e1ab9baa7bcf9e3f6159448ad1a6e6a126d93b
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-08-30 18:55:36 +0530

    toString

--- exercise/src/com/program/collections/tostring_compare/Laptop.java
+++ exercise/src/com/program/collections/tostring_compare/Laptop.java
@@ -6,6 +6,12 @@ public class Laptop {
 	int ram;
 	boolean screen;
 	
+	//to override a toString(). toString() is alreday in Object class so to override a method for my convinience
+	//ithanaala hasscode print panarathuku bathil ah String ah print aagum
+	public String toString() {
+		return " : "+this.price+" : "+this.ram+" : "+this.screen;
+	}
+	
 	public Laptop(int price,int ram,boolean screen) {
 		this.price=price;
 		this.ram=ram;
