c091c421c6f4743a3e3a39914cce2fe5c6355ea4
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-08-30 22:13:40 +0530

    ListIterator

---
+++ exercise/src/com/program/collections/cursors/ListIteratorDemo.java
@@ -0,0 +1,32 @@
+package com.program.collections.cursors;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class ListIteratorDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList al=new ArrayList();
+		al.add("dhaya");
+		al.add("aswini");
+		al.add("naren");
+		al.add("hari");
+		al.add(4);
+		
+		System.out.println("before"+al);
+
+		ListIterator li=al.listIterator();
+		//amma tha frist add aagum because cursor frist la irunthu tha st aagum
+		li.add("amma");
+		
+		while(li.hasNext()) {
+			if(li.next().equals(4)) {
+				li.set(44);;
+			}
+		}
+		System.out.println("after :"+al);
+
+	}
+
+}
