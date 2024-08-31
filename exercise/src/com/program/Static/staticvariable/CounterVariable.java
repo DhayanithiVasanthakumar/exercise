117086586251d52659325173dfd8d6e6089bce8a
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-08-31 10:08:49 +0530

    added question to static variable

--- exercise/src/com/program/Static/staticvariable/CounterVariable.java
+++ exercise/src/com/program/Static/staticvariable/CounterVariable.java
@@ -1,5 +1,11 @@
 package com.program.Static.staticvariable;
 
+/*
+ * Write a Java program to create a class called "Counter" with a static variable count. 
+ * Implement a constructor that increments count every time an object is created.
+ *  Print the value of count after creating several objects.
+ */
+
 public class CounterVariable {
 	
 	private static int count=0;
