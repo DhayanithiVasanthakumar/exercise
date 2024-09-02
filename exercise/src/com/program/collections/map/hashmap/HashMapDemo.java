d719d21a8c986da90e446b70ad44b7ecd7707048
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-09-02 19:40:17 +0530

    HashMap

---
+++ exercise/src/com/program/collections/map/hashmap/HashMapDemo.java
@@ -0,0 +1,33 @@
+package com.program.collections.map.hashmap;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		
+		HashMap hm=new HashMap();
+		//add entry
+		hm.put("dhaya",8);
+		hm.put("aswini", 21);
+		hm.put("naren", 98);
+		
+		
+		//display a entry
+		System.out.println("key + values are :"+hm);
+		
+		//print only keys
+		Set s=hm.keySet();
+		System.out.println("keys are :"+s);
+		
+		//print only values
+		Collection c=hm.values();
+		System.out.println("values are :"+c);
+		
+		//print entrys
+		Set en=hm.entrySet();
+		System.out.println("entries are :"+en);
+	}
+}
