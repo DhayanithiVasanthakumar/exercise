648a988b7d5d4abe464efa351c0f233239169215
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-10-16 10:16:16 +0530

    static method in interface

---
+++ exercise/src/com/program/newfeatures/methods/staticMethod/Contract.java
@@ -0,0 +1,8 @@
+package com.program.newfeatures.methods.staticMethod;
+
+public interface Contract {
+
+	static void test() {
+		System.out.println("from contract interface");
+	}
+}
---
+++ exercise/src/com/program/newfeatures/methods/staticMethod/Employee.java
@@ -0,0 +1,10 @@
+package com.program.newfeatures.methods.staticMethod;
+
+public class Employee implements Contract {
+
+	public static void main(String[] args) {
+		Contract.test();
+
+	}
+
+}
