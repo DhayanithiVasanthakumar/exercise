a20e473c1210ae8a70a3d3b19cb9dfcea4db3b81
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-10-17 20:23:31 +0530

    diffrenciate good and bad approch

--- exercise/src/com/program/newfeatures/stream/Test.java
+++ exercise/src/com/program/newfeatures/stream/Test.java
@@ -1,6 +1,7 @@
 package com.program.newfeatures.stream;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Test {
 
@@ -8,6 +9,22 @@ public class Test {
 		
 		int[] ar= {10,6,3,7,9};
 		
+		/*
+		 * both are same program
+		 */
+		
+		/*
+		 * bad approch
+		
+		IntStream s=Arrays.stream(ar);
+		IntStream s2 =s.sorted();
+		s2.forEach(no -> System.out.println(no));
+		
+		 */
+		
+		/*
+		 * good approch follow this type
+		 */
 		Arrays.stream(ar)
 		.sorted()
 		.forEach(System.out::println);
