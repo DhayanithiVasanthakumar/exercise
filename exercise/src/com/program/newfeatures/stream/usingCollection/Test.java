1043ef8b644e672f8c3cebb6919457b2616a1fc3
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-12-29 21:06:45 +0530

    using hashmap functions like-> compareby key,value and convert map into list/set and filter the data like start with end with letters

--- exercise/src/com/program/newfeatures/stream/usingCollection/Test.java
+++ exercise/src/com/program/newfeatures/stream/usingCollection/Test.java
@@ -1,7 +1,11 @@
 package com.program.newfeatures.stream.usingCollection;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /*
  * as same as using array
@@ -73,6 +77,95 @@ public class Test {
 		 .stream()
 		 .map(name -> name +" B.E...,")
 		 .forEach(System.out::println);
+		 
+		 /*
+		  * additional terminal operations
+		  */
+		 
+		 System.out.println("terminal operations ");
+		 System.out.println();
+		 System.out.println("Reduce");
+		 // it give single result from a sequence of elements
+		 Optional<Integer> result=l
+		 .stream()
+		 .reduce((no1,no2) -> no1+no2);
+		 
+		 System.out.println(result.get());
+		 
+		 System.out.println("........");
+		 
+		 /*
+		  * toArray
+		  */
+		 Object[]  result2=l
+		 .stream()
+		 .toArray();
+		 
+		 for(Object o:result2) {
+			 System.out.println(o);
+		 }
+		 System.out.println("........");
+
+		 /*
+		  * max
+		  */
+		 
+		 Optional<Integer> maxRes= l
+		 .stream()
+		 .max((ele1,ele2) -> ele1.compareTo(ele2));
+		 
+		 System.out.println(maxRes.get());
+		 
+		 System.out.println("........");
+		 
+		 /*
+		  * parallel stream
+		  */
+		 System.out.println("parallel stream");
+		 l
+		 .parallelStream()
+		 .forEachOrdered(System.out::println);
+
+		 System.out.println("........");
+
+		 
+		 /*
+		  * perform opertions in hashmap
+		  */
+		 System.out.println("perform opertions in hashmap");
+		 
+		 HashMap<String,Integer> hm=new HashMap<String,Integer>();
+		 //      key     value
+		 hm.put("dhaya", 100);
+		 hm.put("aswini", 101);
+		 hm.put("naren", 102);
+		 hm.put("hari", 103);
+		 
+		 //direct ah hashmap ah stream pana mudiyathu so atha first entryset ah vangi appram stream panamudiyum
+		 
+		 hm
+		 .entrySet()
+		 .stream()
+		 //sort panum pothu yetha base pani sort pananum nu inga kuduthukalam eg: key ah vachi ah ila vaue ah vachi ah 
+		 .sorted(Map.Entry.comparingByKey())
+		 .forEach(System.out::println);
+		 
+		 
+		//convert map into list/set
+		 List<String> list=  hm
+		 .keySet()
+		 .stream()
+		 .collect(Collectors.toList());//.toSet and etc...
+		 System.out.println(list);
+		 
+		 //filter the data
+		 boolean filterRecord= hm
+		 .keySet()
+		 .stream()
+		 .anyMatch(name -> name.startsWith("d"));
+		 
+		 System.out.println(filterRecord);
+		 
 	}
 
 }
