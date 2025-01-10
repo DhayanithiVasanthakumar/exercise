cc6ad7dcbf27195cb92ab964556dfcf103fab210
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2025-01-10 20:32:24 +0530

    Runnable,Running

--- exercise/src/com/program/multithreading/MainThread.java
+++ exercise/src/com/program/multithreading/MainThread.java
@@ -6,9 +6,20 @@ public class MainThread {
 		ChildThread ct=new ChildThread();
 		ct.start();
 		
+	
+		
 		for(int i=0;i<=5;i++) {
 			System.out.println("Main thread "+i);
 		}
+		
+		//getClass() method
+		System.out.println(ct.getName());
+		//setClass() method
+		ct.setName("check class");
+		System.out.println(ct.getName());
+		
+		System.out.println(ct.getPriority());
+		ct.setPriority(1);
 	}
 
 }
