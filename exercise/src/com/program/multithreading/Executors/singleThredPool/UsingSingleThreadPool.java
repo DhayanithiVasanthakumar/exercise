450a3759ee323b9d5a9416145701ea035e7e568b
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2025-01-13 19:18:14 +0530

    singleThreadPool

---
+++ exercise/src/com/program/multithreading/Executors/singleThredPool/UsingSingleThreadPool.java
@@ -0,0 +1,19 @@
+package com.program.multithreading.Executors.singleThredPool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class UsingSingleThreadPool {
+
+	public static void main(String[] args) {
+		System.out.println("Main thread starts ");
+		ExecutorService es = Executors.newSingleThreadExecutor();
+
+		// inga i<3 ku meaning 3 task
+		for (int i = 0; i < 3; i++) {
+			es.execute(new LoopTest());
+		}
+		es.shutdown();
+		System.out.println("Main thread end");
+	}
+}
